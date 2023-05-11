35087f6c028af5737c91006f3419749167b3db9b
koechronix <koechronix@yahoo.com>
2023-05-11 22:29:20 +0300

    Added code to payment check vue

---
+++ src/components/PaymentCheckoutForm.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { computed, ref } from "vue";
+import { Show } from "../shared/types";
+import Checkout from "./checkout.vue";
+import Payment from "./Payment.vue";
+
+defineProps<{ shows: Show[] }>();
+</script>
+
+<template>
+  <form class="grid grid-cols-1 md:grid-cols-2">
+    <Payment />
+    <Checkout :shows="shows" />
+  </form>
+</template>
+
+<style scoped>
+</style>
