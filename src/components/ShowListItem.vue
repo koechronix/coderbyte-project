658d908d02846be0b0b0ed2e2500e1dec4818f97
koechronix <koechronix@yahoo.com>
2023-05-11 22:30:15 +0300

    Added code to showlist vue

---
+++ src/components/ShowListItem.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { computed, ref } from "vue";
+import { Show } from "../shared/types";
+
+const props = defineProps<{ show: Show }>();
+
+const incrementCount = () => {
+  props.show.count++;
+};
+
+const decrementCount = () => {
+  if (props.show.count === 0) return;
+  props.show.count--;
+};
+</script>
+
+<template>
+  <li class="flex flex-row bg-indigo-800 rounded-md p-2">
+    <div class="flex-grow">
+      <h2 class="text-lg">
+        {{ show.title
+        }}<span class="ml-2 text-sm text-indigo-300"
+          >({{ show.location }})</span
+        >
+      </h2>
+      <p class="text-gray-400">{{ show.subtitle }}</p>
+    </div>
+    <div class="flex flex-col">
+      <div class="text-center">${{ show.price.toFixed(2) }}</div>
+      <div class="text-center">
+        <button @click="decrementCount">
+          <i class="bi bi-dash-circle"></i>
+        </button>
+        {{ show.count }}
+        <button @click="incrementCount">
+          <i class="bi bi-plus-circle"></i>
+        </button>
+      </div>
+    </div>
+  </li>
+</template>
+
+<style scoped>
+</style>
